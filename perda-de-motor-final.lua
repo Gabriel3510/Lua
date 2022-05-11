49ec75685f349f3e00b6514a916b1df17b27a836
Gabriel Lima <gabriel.lima@speedbird.aero>
2022-05-11 16:50:31 -0300

    Perda de motor

--- perda-de-motor-final.lua
+++ perda-de-motor-final.lua
@@ -1,9 +1,13 @@
+local motor1 = 5
+local motor2 = 4
+local motor3 = 2
+local motor4 = 3
+local motor5 = 0
+local motor6 = 1
 function update()
   if rc:get_pwm(6) > 1700 then --get_pwm seta qual canal do radio vai funcionar.
-    SRV_Channels:set_output_pwm_chan_timeout(0,1000,15) -- aqui vc determinda qual motor vai parar e pra qual pwm ele vai.
+    SRV_Channels:set_output_pwm_chan_timeout(motor1,1000,15) -- aqui voce determinda qual motor vai parar e pra qual pwm ele vai.
   end
   return update, 10 --
-  
-
 end
 return update() -- run immediately before starting to reschedule
